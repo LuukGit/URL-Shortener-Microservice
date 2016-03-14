33f0334ef8d96a0582e240a8bb8b18c9930c54c3
Luuk <luukvoorbrowsen@gmail.com>
2016-03-14 18:29:11 +0000

    FINISHED

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-/*
-  User Stories:   
-    - I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.  
-    - If I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
-    - When I visit that shortened URL, it will redirect me to my original link.
-  
-  Example output:
-    { "original_url": "http://freecodecamp.com/news", "short_url": "https://luuk-url-shortener-ms.herokuapp.com/4" } 
-*/
-
 var path = require("path");
 var http = require('http');
 var express = require('express');
