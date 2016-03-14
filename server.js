5485dd2ce60bf9e4eb62307a0627d0964b2c774f
Luuk <luukvoorbrowsen@gmail.com>
2016-03-14 16:12:59 +0000

    Fixed bugs

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ var mongo = require("mongodb").MongoClient;
 var app = express();
 var server = http.createServer(app);
 
-var URL = "https://luuk-url-shortener-ms.herokuapp.com/";
+var URL = "https://url-shortener-microservice-luukgit.c9users.io";
 var mongoURL = "mongodb://client:client@ds013579.mlab.com:13579/freecodecamp-database";
 
 app.use(express.static(path.resolve(__dirname, "client")));
 
-app.get("/:query", function(req, res) {
-    var short_url = URL + req.params.query;
+app.get("/:short", function(req, res) {
+    var short_url = URL + req.url;
     // Search the database for the original_url using the short_url.
     mongo.connect(mongoURL, function(err, db) {
         if (err) { throw err; }
@@ -32,7 +32,6 @@ app.get("/:query", function(req, res) {
             short_url: short_url
         }).toArray(function(err, documents) {
             if (err) { throw err; }
-            console.log(documents);
             original_url = documents[0].original_url;
             res.redirect(original_url);
             db.close();
@@ -42,14 +41,13 @@ app.get("/:query", function(req, res) {
 
 app.get("/new/:query*", function(req, res) {
     var original_url = req.url.replace("/new/", "");
-    console.log(original_url);
     // Make sure the URL has the right format ("http://www.example.com"), as specified in the FCC instruction video. 
     // Add missing http// or https//. 
-    if (!original_url.match(/http:\/\//) && !original_url.match(/https:\/\//) )
+    if (!original_url.match(/^http:\/\/.+$/) && !original_url.match(/^https:\/\/.+$/) )
     {
       original_url = "http://" + original_url;
     }
-    // Return error if the URL doesn't have at least one dot.
+    // Handle request if the original URL has at least 1 dot. Else return error.
     if (original_url.match(/\w\.\w/g))
     { 
       // Create the new short_url and add it to the database.  
@@ -62,14 +60,13 @@ app.get("/new/:query*", function(req, res) {
         // Get the number for the new short_url by counting the previous short_url entries.
         collection.count({}, function(err, count) {
             if (err) { throw err; }
-            short_url = URL + count;
+            short_url = URL + "/" + count;
             // Insert the new original/short pair.
             collection.insert({
                 original_url: original_url,
                 short_url: short_url
             }, function(err, result) {
                     if (err) { throw err };
-                    console.log(JSON.stringify(result.ops[0]));
                     db.close();
                     res.send({original_url: original_url, short_url: short_url});
             });
